724e850d4c902e7182d35be2b7cff24eed6ea4b6
chinhnvc98 <vanchinhpc9.com>
2020-12-08 17:28:46 +0700

    update graphQl to get author

---
+++ Model/Resolver/Author.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Copyright © Landofcoder All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Lof\BlogGraphQl\Model\Resolver;
+
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Ves\Blog\Api\AuthorRepositoryInterface;
+
+class Author implements ResolverInterface
+{
+
+
+    /**
+     * @var AuthorRepositoryInterface
+     */
+    private $authorManagement;
+
+    public function __construct(
+        AuthorRepositoryInterface $authorManagement
+    )
+    {
+        $this->authorManagement = $authorManagement;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        if (!$this->authorManagement->get($args['author_id'])) {
+            throw new GraphQlInputException(__('Author Id does not match any Author.'));
+        }
+        return $this->authorManagement->get($args['author_id']);
+    }
+}
